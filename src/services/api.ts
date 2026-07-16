2e1695f316427187b648691283ccc61bf864ac24
qtechx001 <qtechx001@gmail.com>
2026-07-16 10:16:05 +0530

    link change fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const API_BASE_URL = 'https://priyamsupermarket.qtechx.com/api';
+export const API_BASE_URL = 'https://supermarket.qtechx.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
