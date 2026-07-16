ece4e9f241200dbbc0b8fa40303c316c3e34e030
qtechx001 <qtechx001@gmail.com>
2026-07-16 10:38:57 +0530

    Account pages created

---
+++ src/pages/Account/Orders.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const Profile = () => {
+  return (
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-2xl font-bold text-green-700">Orders</Text>
+    </View>
+  );
+};
