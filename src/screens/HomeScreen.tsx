84a6293f2f73970fd8ddee90c174053181b97e02
qtechx001 <qtechx001@gmail.com>
2026-07-16 17:16:21 +0530

    Pages Created

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,15 +4,20 @@ import { CategorySection } from '../components/CategorySection';
 import { AboutSection } from '../components/AboutSection';
 import { Features } from '../components/Feature';
 import { Banner1 } from '../components/Banner1';
+import { Hero } from '../components/Hero';
+import { ProductSwiper } from '../components/ProductsSwiper';
 
 export const HomeScreen = () => {
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="py-4">
+        <Hero />
         <CategorySection />
+        <ProductSwiper />
         <AboutSection />
         <Banner1/>
         <Features />
+
       </View>
     </ScrollView>
   );
