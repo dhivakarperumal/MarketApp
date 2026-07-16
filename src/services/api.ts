339176b19c541def989cc0db36d99b0024bbc2df
qtechx001 <qtechx001@gmail.com>
2026-07-16 17:23:27 +0530

    link changing fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// export const API_BASE_URL = 'https://supermarket.qtechx.com/api';
 export const API_BASE_URL = 'http://192.168.1.2:5000/api';
 
 const api = axios.create({
