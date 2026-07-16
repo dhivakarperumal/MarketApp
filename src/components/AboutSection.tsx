1c21199f09dc59379fe16541c1be0c3c19f2d3cf
qtechx001 <qtechx001@gmail.com>
2026-07-16 11:52:35 +0530

    About Section page

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const AboutSection = () => {
+  return (
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-2xl font-bold text-green-700">About Screen</Text>
+    </View>
+  );
+};
