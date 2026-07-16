ede86827ff74206d97d9e7239d8e418234f9acd2
qtechx001 <qtechx001@gmail.com>
2026-07-16 16:08:36 +0530

    Tab Navigator Design Fix

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -8,122 +8,46 @@ import { ComboScreen } from '../screens/ComboScreen';
 import { MoreScreen } from '../screens/MoreScreen';
 import { Header } from '../components/Header';
 import { Home, Package, ShoppingCart, Layers, Menu } from 'lucide-react-native';
+import CustomTabBar from "./CustomTabBar";
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
   return (
     <Tab.Navigator
+      tabBar={(props) => <CustomTabBar {...props} />}
       screenOptions={({ route }) => ({
         header: () => <Header title={route.name} />,
-
-        tabBarShowLabel: true,
-        tabBarHideOnKeyboard: true,
-
-        tabBarActiveTintColor: "#16A34A",
-        tabBarInactiveTintColor: "#94A3B8",
-
-        tabBarLabelStyle: {
-          fontSize: 11,
-          fontWeight: "600",
-          marginBottom: 4,
-        },
-
-        tabBarStyle: {
-          position: "absolute",
-          left: 16,
-          right: 16,
-          bottom: 16,
-          height: 72,
-          borderRadius: 22,
-          backgroundColor: "#FFFFFF",
-          borderTopWidth: 0,
-          elevation: 12,
-          shadowColor: "#000",
-          shadowOpacity: 0.12,
-          shadowRadius: 16,
-          shadowOffset: {
-            width: 0,
-            height: 8,
-          },
-        },
-
-        tabBarItemStyle: {
-          paddingVertical: 6,
-        },
-
-        tabBarIcon: ({ color, focused }) => {
-          const size = focused ? 24 : 22;
-
-          let Icon = Home;
-
-          switch (route.name) {
-            case "Home":
-              Icon = Home;
-              break;
-            case "Products":
-              Icon = Package;
-              break;
-            case "Cart":
-              Icon = ShoppingCart;
-              break;
-            case "Combo":
-              Icon = Layers;
-              break;
-            case "More":
-              Icon = Menu;
-              break;
-          }
-
-          return (
-            <View
-              style={{
-                width: focused ? 44 : 40,
-                height: focused ? 44 : 40,
-                borderRadius: 22,
-                backgroundColor: focused ? "#DCFCE7" : "transparent",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Icon
-                size={size}
-                color={focused ? "#16A34A" : "#94A3B8"}
-                strokeWidth={2.4}
-              />
-            </View>
-          );
-        },
       })}
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ tabBarLabel: "Home" }}
+        options={{ tabBarLabel: "" }}
       />
 
       <Tab.Screen
         name="Products"
         component={ProductsScreen}
-        options={{ tabBarLabel: "Products" }}
+        options={{ tabBarLabel: "" }}
       />
 
       <Tab.Screen
         name="Cart"
         component={CartScreen}
-        options={{ tabBarLabel: "Cart" }}
+        options={{ tabBarLabel: "" }}
       />
 
       <Tab.Screen
         name="Combo"
         component={ComboScreen}
-        options={{ tabBarLabel: "Combo" }}
+        options={{ tabBarLabel: "" }}
       />
 
       <Tab.Screen
         name="More"
         component={MoreScreen}
-        options={{ tabBarLabel: "More" }}
+        options={{ tabBarLabel: "" }}
       />
     </Tab.Navigator>
   );
