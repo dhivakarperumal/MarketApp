145fb3fad514fdbf135be7d40c101b69d7f7c894
qtechx001 <qtechx001@gmail.com>
2026-07-21 16:02:55 +0530

    api ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const API_BASE_URL = 'http://192.168.1.2:5000/api';
+export const API_BASE_URL = 'http://192.168.1.8:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
