d15f5fec01de814d431c70f126f971876bf70940
qtechx001 <qtechx001@gmail.com>
2026-07-22 16:32:06 +0530

    Main Activity navigation bar fix

--- android/app/src/main/java/com/supermarketapp/MainActivity.kt
+++ android/app/src/main/java/com/supermarketapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.supermarketapp
 
+import android.os.Bundle
+import android.graphics.Color
+import android.view.View
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+    window.navigationBarColor = Color.WHITE
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+      @Suppress("DEPRECATION")
+      window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+    }
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
