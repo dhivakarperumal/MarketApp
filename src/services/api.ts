da0eff5fffea311208b2557485a5736f788e7fac
qtechx001 <qtechx001@gmail.com>
2026-07-23 12:50:52 +0530

    api fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const API_BASE_URL = 'https://dmart.qtechx.com/api';
+// export const API_BASE_URL = 'https://dmart.qtechx.com/api';
+
+export const API_BASE_URL = 'http://192.168.1.8:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
